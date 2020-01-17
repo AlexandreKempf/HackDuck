724bbfa8c658b9e0c5d6c0b33a8011bf6676b08a
alexandre <alexandre@pandascore.co>
2020-01-17 14:50:33 +0100

    setup for pip package

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+packages = ["HackDuck"]
+
+setuptools.setup(
+    name="HackDuck",
+    version="0.1.0",
+    author="Alexandre Kempf",
+    author_email="alexandre.kempf@cri-paris.org",
+    description="Machine learning data flow for reproducible data science",
+    long_description=long_description,
+    package_dir={"": "src"},
+    packages=["HackDuck"],
+    long_description_content_type="text/markdown",
+    url="https://github.com/AlexandreKempf/HackDuck",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
